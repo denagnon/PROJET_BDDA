c38bb42b6ed4bad920e289d417bc24b17b91c020
SMNO971 <samuelnoel4e5@gmail.com>
2026-01-02 21:06:30 +0100

    Config: Augmentation mémoire (2Go) pour supporter les scénarios Big Data

--- app.sh
+++ app.sh
@@ -2,4 +2,4 @@
 # Lancement du SGBD
 # Usage attendu : ./app.sh <FichierConfig> <FichierScenario>
 # Exemple : ./app.sh config.txt commande_scenario.txt
-java -cp bin sgbd.Main "$@"
+java -Xmx2g -cp bin sgbd.Main "$@"
